05c1caf1b8139d83b54c7af8610b75474ec4ae70
P@Git147b <pablosg.disegno@gmail.com>
2022-11-03 06:55:41 +0100

    configuranto CI con Github Actions

--- src/common/components/spinner/spinner.component.spec.tsx
+++ src/common/components/spinner/spinner.component.spec.tsx
@@ -2,26 +2,27 @@ import React from 'react';
 import { usePromiseTracker, trackPromise } from 'react-promise-tracker';
 import { render, screen } from '@testing-library/react';
 import { SpinnerComponent } from './spinner.component';
+import { isValidLogin } from '../../../pods/login/login.api';
 import Loader from 'react-spinners/ScaleLoader';
 import { Modal } from '@mui/material';
 import { renderHook, act } from '@testing-library/react';
 
 describe('SpinnerComponent component specs', () => {
-  it('should display spinner loader', () => {
+  it('should display spinner loader by default to false', async () => {
     // Arrange
-   const { result } = renderHook(() => usePromiseTracker())
 
-   const defaultPromiseInProgress = false
+   const { result } = renderHook(() => usePromiseTracker())
+   const defaultPromiseInProgress = await trackPromise(isValidLogin("admin","test"))
 
     // Act
     render(<SpinnerComponent />);
 
-    //const modalElement = screen.getByRole('presentation');
+    const modalElement = screen.getByRole('presentation');
 
     screen.debug();
 
     // Assert
     expect(result.current.promiseInProgress).toEqual(defaultPromiseInProgress);
-    //expect(modalElement).toBeInTheDocument()
+    expect(modalElement).toBeInTheDocument()
   });
 });
